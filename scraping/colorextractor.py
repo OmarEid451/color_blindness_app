c43536056ae4f5a2711e73a3e8bf839d9249d6a0
OmarEid451 <omareid1917@gmail.com>
2024-01-27 17:40:02 -0800

    extracted hex values

--- scraping/colorextractor.py
+++ scraping/colorextractor.py
@@ -4,12 +4,16 @@ fhand = open("colorpage.html")
 soup = BeautifulSoup(fhand, "html.parser")
 color_values = soup.find_all("p", title=True)
 color_names = soup.find_all("p", style=True)
-#for tag in color_values:
- #   print(tag["title"])
-for tag in color_names:
+for tag in color_values:
+    values = tag['title'].split()
+    hex_color = values[-1]
+    hex_color = hex_color[1:]
+    print(hex_color)
+
+
+#extract color names
+'''for tag in color_names:
     color = str(tag.contents[0].string)
     color = color.strip()
-    print(color)
-    #print(tag.contents[0])
+    print(color)'''
 fhand.close()
-#print(color_tables)
