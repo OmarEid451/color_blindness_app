d1e5e79cd49a9232850446a47623d039d8324a13
OmarEid451 <omareid1917@gmail.com>
2024-01-27 16:29:40 -0800

    successfully extracted color names

--- scraping/colorextractor.py
+++ scraping/colorextractor.py
@@ -1,7 +1,15 @@
 from bs4 import BeautifulSoup
 
-
-soup = BeautifulSoup(open("colorpage.html"), "html.parser")
-color_tables = soup.find_all("div", style=True)
-print(color_tables)
-
+fhand = open("colorpage.html")
+soup = BeautifulSoup(fhand, "html.parser")
+color_values = soup.find_all("p", title=True)
+color_names = soup.find_all("p", style=True)
+#for tag in color_values:
+ #   print(tag["title"])
+for tag in color_names:
+    color = str(tag.contents[0].string)
+    color = color.strip()
+    print(color)
+    #print(tag.contents[0])
+fhand.close()
+#print(color_tables)
