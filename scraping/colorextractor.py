6cefc7a12be56fef2910dc44ebaaa3facf259667
OmarEid451 <omareid1917@gmail.com>
2024-01-27 18:23:53 -0800

    fix name-hex mismatch

--- scraping/colorextractor.py
+++ scraping/colorextractor.py
@@ -4,16 +4,23 @@ fhand = open("colorpage.html")
 soup = BeautifulSoup(fhand, "html.parser")
 color_values = soup.find_all("p", title=True)
 color_names = soup.find_all("p", style=True)
+hex_doc = open("hex_values.txt", 'w')
+color_name_doc = open("color_names.txt", 'w')
+
+# extract hex_values
 for tag in color_values:
     values = tag['title'].split()
     hex_color = values[-1]
     hex_color = hex_color[1:]
-    print(hex_color)
-
+    hex_doc.write(hex_color)
+    hex_doc.write('\n')
+hex_doc.close()
 
 #extract color names
-'''for tag in color_names:
+for tag in color_names:
     color = str(tag.contents[0].string)
     color = color.strip()
-    print(color)'''
+    color_name_doc.write(color)
+    color_name_doc.write('\n')
+color_name_doc.close()
 fhand.close()
