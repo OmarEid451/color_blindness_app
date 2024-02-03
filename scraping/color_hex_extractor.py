5cbf160ed3011edb561ce873dad9aa73b8a79cc5
OmarEid451 <omareid1917@gmail.com>
2024-02-02 17:10:04 -0800

    corrected JSON to have RGB colors

--- scraping/color_hex_extractor.py
+++ scraping/color_hex_extractor.py
@@ -6,8 +6,12 @@ json_handle = open("colors.json", 'w')
 soup = BeautifulSoup(webpage, "html.parser")
 colors = soup.find_all("td", style=True)
 for tag in colors:
+    rgb_values = []
     hex_num = tag['style'].split(':')[1][1:-1]
-    color_dict[hex_num] = tag.text
+    for i in range(0, len(hex_num), 2):
+        rgb_num = int(hex_num[i:i+2], 16)
+        rgb_values.append(rgb_num)
+    color_dict[tag.text] = rgb_values
 
 json.dump(color_dict, json_handle, indent=4)
 webpage.close()
