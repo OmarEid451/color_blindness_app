fd91c1ba3db3e28f9f51bd42c3f0ebf3572bcd91
OmarEid451 <omareid1917@gmail.com>
2024-02-07 15:32:29 -0800

    image prototype working

--- main/index.js
+++ main/index.js
@@ -1,18 +1,24 @@
 //test to see if I can auto render an image from a browser function
-function loadImage(image_element) {
-    // TODO fix urlobject_failure
-    image_element.src = URL.createObjectURL(this.files);
+function loadImage(event) {
+    
+     /* look at this link for reference https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications */
+    // now we need to create a canvas using the loaded image: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
+    image_space.innerHTML = "";
+    image_element = document.createElement("img");
+    image_element.src = URL.createObjectURL(event.target.files[0]);
     image_element.onload = function() {
         URL.revokeObjectURL(image_element.src);
     };
+    image_space.appendChild(image_element);
+    
 }
 
 
-// get both the input and image tags from the page.
+// get both the input and div tags from the page.
 file = document.getElementById("picture");
-image_tag = document.getElementById("current_image");
+image_space = document.getElementById("current_image");
 
-file.addEventListener("change", loadImage(image_tag));
+file.addEventListener("change", loadImage);
 
 
     
